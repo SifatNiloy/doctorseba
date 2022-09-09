959120b00678f934aae06c849ab370eb7345c0fc
unknown <sifatniloy18@gmail.com>
2022-09-09 00:29:35 -0600

    navbar top-fixed

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -1,18 +1,21 @@
 import React from 'react';
 import { Container, Nav, Navbar } from 'react-bootstrap';
-import logo from '../../../images/logo-2.png'
+import logo from '../../../images/logo-1.png'
+import './Header.css'
 const Header = () => {
     return (
         <>
-            <Navbar bg="primary" variant="dark">
+            <Navbar bg="primary" variant="dark" className='top-navbar' fixed="top">
                 <Container>
                     <Navbar.Brand href="#home">
-                        <img src={logo} height="30px" alt="" />
+                        <img height={'30px'} src={logo} alt="" />
                     </Navbar.Brand>
                     <Nav className="me-auto">
                         <Nav.Link href="#home">Home</Nav.Link>
-                        <Nav.Link href="#features">Features</Nav.Link>
-                        <Nav.Link href="#pricing">Pricing</Nav.Link>
+                        <Nav.Link href="#features">Services</Nav.Link>
+                        <Nav.Link href="#pricing">Contact us</Nav.Link>
+                        <Nav.Link href="#pricing">About us</Nav.Link>
+                        <Nav.Link href="#pricing">Sign In</Nav.Link>
                     </Nav>
                 </Container>
             </Navbar>
