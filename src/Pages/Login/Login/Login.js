4c3607805e106a1baccf3f3b4904a191299bae85
unknown <sifatniloy18@gmail.com>
2022-09-11 11:49:01 -0600

    login working

--- src/Pages/Login/Login/Login.js
+++ src/Pages/Login/Login/Login.js
@@ -1,16 +1,28 @@
 import React, { useRef } from 'react';
 import { Button, Form } from 'react-bootstrap';
+import { useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
 import { Link, useNavigate } from 'react-router-dom';
+import auth from '../../../firebase.init';
 import './Login.css';
 const Login = () => {
     const emailRef = useRef('');
     const passwordRef = useRef('');
     const navigate = useNavigate();
+    const [
+        signInWithEmailAndPassword,
+        user,
+        loading,
+        error,
+    ] = useSignInWithEmailAndPassword(auth);
+    if (user) {
+        navigate('/home')
+    }
     const handleSubmit = event => {
         event.preventDefault();
         const email = emailRef.current.value;
         const password = passwordRef.current.value;
         console.log(email, password);
+        signInWithEmailAndPassword(email, password);
     }
     const navigateRegister = event => {
         navigate('/register');
@@ -20,9 +32,9 @@ const Login = () => {
             <h2 className='text-primary text-center'>Please login</h2>
             <form onSubmit={handleSubmit}>
                 {/* <input type="text" name="name" placeholder='name' required /> */}
-                <input type="email" name="email" placeholder='email' required />
-                <input type="password" name="password" placeholder='password' required />
-                <input type="submit" value="Register" />
+                <input type="email" ref={emailRef} name="email" placeholder='email' required />
+                <input type="password" ref={passwordRef} name="password" placeholder='password' required />
+                <input className='submit-button' type="submit" value="Login" />
             </form>
             {/* <Form onSubmit={handleSubmit}>
                 <Form.Group className="mb-3" controlId="formBasicEmail">
